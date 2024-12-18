b25895c6b4f6068c33e6d41cb2d3c6606f3615e8
Sundaravelan-dotcom <sundaravelan.2020@vitstudent.ac.in>
2024-12-18 23:07:00 +0530

    Updates

---
+++ precache-manifest.d68b22761eb7aa757ef12d28eed4fd25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87fdba74312a1f56511e29b3491ff22b",
+    "url": "/Notes-App/index.html"
+  },
+  {
+    "revision": "820a5c81b8baa2935607",
+    "url": "/Notes-App/static/css/main.6b1265a1.chunk.css"
+  },
+  {
+    "revision": "ada6c78fdd4a8eb5acc3",
+    "url": "/Notes-App/static/js/2.cfef5e3c.chunk.js"
+  },
+  {
+    "revision": "820a5c81b8baa2935607",
+    "url": "/Notes-App/static/js/main.44b0b934.chunk.js"
+  },
+  {
+    "revision": "a8896799ec7a26d49e79",
+    "url": "/Notes-App/static/js/runtime~main.3a05fdbf.js"
+  }
+]);
